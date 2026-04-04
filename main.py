d177359328c4eb7c9a2b0ffdaaf77b9c6d0f0299
SadikMuktadir <sadikmuktadir2.0@gmail.com>
2026-04-04 21:34:30 +0600

    upto test train data

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+import pandas as pd
 
+rainfall_data = pd.read_csv("data/rainfall_data_2015_2025.csv")
+
+rainfall_data["rainfall_lag_1"]=rainfall_data["rainfall"].shift(1)
+rainfall_data["rainfall_lag_2"]=rainfall_data["rainfall"].shift(2)
+rainfall_data["rainfall_lag_3"]=rainfall_data["rainfall"].shift(3)
+
+
+train_rainfall_data = rainfall_data[:"2018"]
+test_rainfall_data = rainfall_data["2019":]
 
